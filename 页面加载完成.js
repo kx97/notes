0c1ab8c45513d7b5007a812a84dfe1f0d5b67257
kx97 <1425236749@qq.com>
2019-02-16 20:50:43 +0800

    start

---
+++ 页面加载完成.js
@@ -0,0 +1,17 @@
+// 定义document.ready()
+document.ready = function(callback) {
+  // 标准浏览器适用
+  if(document.addEventListener) {
+    document.addEventListener("DOMContentLoaded", callback, false);
+  } else if(document.attachEvent) {
+    // ie8 及以下浏览器适用
+    document.attachEvent('onreadystatechange', function () { 
+      // 已加载 文档和用户可以开始交互
+      if(document.readyState == 'interactive') {
+        callback(window.event);
+      }
+    });
+  } else {
+    window.onload = callback;
+  }
+};
