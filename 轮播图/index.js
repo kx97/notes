4de49a45c1b7c6f28dc1d66da61666dbd039feb4
kx97 <1425236749@qq.com>
2019-02-22 19:46:01 +0800

    5

--- 轮播图/index.js
+++ 轮播图/index.js
@@ -1,98 +1,97 @@
 (function() {
-  window.onload = function() {
-      //图片自动播放
-      autoslide();
-      //点击左右按钮，切换图片 
-      var prev = document.querySelector('.prev');
-      var next = document.querySelector('.next');
-      prev.onclick = function() {
-          slideprev();
-          autoslide();
-      };
-      next.onclick = function() {
-          slidenext();
-          autoslide();
-      };
-      // 点击小圆点，切换图片
-      //使用 委托
-      var slideDiv = document.querySelector('.slide-sel');
-      slideDiv.onclick = function(e) {
-          e = e || window.event;
-          var target = e.target || e.srcElement;
-          if(target == this) {
-              return;
-          } else {
-              slidesel(target);
-              autoslide();
-          }
-      };
-      //实现的相关函数
-      var timer;
-      function autoslide() {
-          if(timer) {
-              clearInterval(timer);
-              timer = null;
-          }
-          timer = setInterval(function() {
-              slidenext();
-          }, 2000);
-      }
-      function slideprev() {
-          var curimg = document.querySelector('.img-list .on');
-          var curindex = parseInt(curimg.getAttribute('index'));
-          
+    window.onload = function() {
+        autoplay();
+        var prev = document.querySelector(".prev");
+        var next = document.querySelector('.next');
+        prev.onclick = function() {
+            slideprev();
+            autoplay();
+        }
+        next.onclick = function() {
+            slidenext();
+            autoplay();
+        };
+        // 把a的点击事件委托给父盒子
+        var sel = document.querySelector('.slide-sel');
+        sel.onclick = function(e) {
+            e = e || window.event;
+            var target = e.target || e.srcElement;
+            if(target === this) {
+                return;
+            } else {
+                slidesel(target);
+                autoplay();
+            }
+        };
+        // 自动播放
+        var timer;
+        function autoplay() {
+            if(timer) {
+                clearInterval(timer);
+                timer = null;
+            }
+            timer = setInterval(function() {
+                slidenext();
+            }, 2000);
+        }
+        // 滑到下一张
+        function slidenext() {
+            var imgList = document.querySelector(".img-list");
+            var curimg = imgList.querySelector(".on");
+            var curindex = parseInt(curimg.getAttribute('index'));
+            var nextindex =  (curindex + 1) % 4;
+            slide(curindex, nextindex);
+        }
+        // 滑到上一张
+        function slideprev() {
+            var imgList = document.querySelector(".img-list");
+            var curimg = imgList.querySelector('.on');
+            var curindex = parseInt(curimg.getAttribute('index'));
+            var nextindex = (curindex + 3) % 4;
+            slide(curindex, nextindex);
+        }
+        // 选中圆点滑动
+        function slidesel(target) {
+            var imgList = document.querySelector(".img-list");
+            var curimg = imgList.querySelector('.on');
+            var curindex = parseInt(curimg.getAttribute('index'));
+            var nextindex = parseInt(target.getAttribute('index'));
+            slide(curindex, nextindex);
+        }
+        // 实现滑动函数
+        function slide(curindex, nextindex) {
+            var imgList = document.querySelector(".img-list");
+            var selitems = document.querySelectorAll(".slide-sel a");
+            var imgItems = imgList.querySelectorAll('li');
 
-          var nextindex = (curindex + 3) % 4;
-          slide(curindex, nextindex);
-      }
-      function slidenext() {
-          var curimg = document.querySelector('.img-list .on');
-          var curindex = parseInt(curimg.getAttribute('index'));
+            var curleft = imgList.offsetLeft;
+            var nextLeft = (-500) * nextindex;
 
-          var nextindex = (curindex + 1) % 4;
-          slide(curindex, nextindex);
-      }
-      function slidesel(target) {
-          var curindex = document.querySelector('.img-list .on').getAttribute('index');
-          curindex = parseInt(curindex);
-          var nextindex = target.getAttribute('index');
-          nextindex = parseInt(nextindex);
+            slideslow(curleft, nextLeft, imgList, 1000);
+            // 设置图片的className
+            imgItems[curindex].className = "";
+            imgItems[nextindex].className = 'on';
+            // 设置sel 的className
+            selitems[curindex].className = '';
+            selitems[nextindex].className = 'on';
 
-          slide(curindex, nextindex);
-      }
-      function slide(curindex, nextindex) {
-          var imglist = document.querySelector('.img-list');
-          var curleft = imglist.offsetLeft;
-          var nextleft = nextindex * (-500);
-
-          //imglist.style.left = nextleft + 'px';
-          slideslow(curleft, nextleft, imglist, 800);
-          var lilist = imglist.querySelectorAll('li');
-          lilist[curindex].className = '';
-          lilist[nextindex].className = 'on';
-
-          var sellist = document.querySelectorAll('.slide-sel a');
-          sellist[curindex].className = '';
-          sellist[nextindex].className = 'on';
-      }
-      //实现图片接连滑出
-      function slideslow(curleft, nextleft, element, duration) { 
-          var distance = nextleft - curleft;
-          var distanceMS = distance / duration;
-          var start = Date.now();
-
-          var distimer = setInterval(function() {
-              var now = Date.now();
-              var Dtime = now - start;
-              element.style.left = (element.offsetLeft + Dtime * distanceMS) + 'px';
-              duration = duration - Dtime;
-              if(duration <= 0) {
-                  element.style.left = nextleft + 'px';
-                  clearInterval(distimer);
-                  return;
-              }
-              now = start;
-          }, 1000/60);
-      }
-  };
+        }
+        // 滑动效果函数
+        function slideslow(curleft, nextLeft, element, duration) {
+            var length = nextLeft - curleft;
+            var lengthMS = length / duration;
+            var start = Date.now();
+            var slideTimer = setInterval(function() {
+                var now = Date.now();
+                element.style.left = (element.offsetLeft + lengthMS * (now - start)) + 'px';
+                duration -= now - start;
+                if(duration <= 0) {
+                    element.style.left = nextLeft + 'px';
+                    clearInterval(slideTimer);
+                    return;
+                }
+                now = start;
+            }, 1000/60);
+        }
+    };
 })();
